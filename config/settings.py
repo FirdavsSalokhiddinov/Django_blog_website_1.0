e5062a6ba7932ee44c469045740ca7f024112e2d
Firdavs Salokhiddinov <firdavssalokhiddinov07@gmail.com>
2023-05-02 01:21:05 +0500

    'ready-site'

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4#)-rppfk^2n@3i=hglpd4!vxane_wczw)^x$bxtrw$zyc9m0k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
